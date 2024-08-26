d95828b3a7c74d662c01b3fa1b01e9627405056b
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-26 16:59:31 +0300

    coin component

---
+++ src/components/Coin.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function Coin({ selectedSide, flipCoin, wallet,result }) {
+  const [isFlipping, setIsFlipping] = useState(false);
+  const [flippedSide, setFlippedSide] = useState(null);
+
+  const handleFlipCoin = () => {
+    setIsFlipping(true);
+
+    setTimeout(() => {
+      const result = flipCoin(); // This should return 'Heads' or 'Tails'
+      setFlippedSide(result); // Set the flipped side to the result of the coin flip
+      setIsFlipping(false);
+    }, 1000); // Match the timeout with the duration of the animation
+  };
+console.log(flippedSide)
+  return (
+    <div>
+      <div>
+        <h2>Choose a side:</h2>
+        <p className='text-white text-xl'>{selectedSide ? `You chose: ${selectedSide}` : 'Click the flip button to start'}</p>
+        <img
+          src={`/${result|| selectedSide}.jpg`} // Show the flipped side if available, otherwise the selected side
+          alt={flippedSide || selectedSide}
+          className={`w-36 h-36 rounded-full ${isFlipping ? 'flip-animation' : ''}`}
+        />
+      </div>
+      <button
+        className="px-8 m-5 py-3 bg-stone-950 text-white rounded-md"
+        onClick={flipCoin}
+        disabled={!wallet.connected || isFlipping}
+      >
+        Flip Coin
+      </button>
+    </div>
+  );
+}
+
+export default Coin;
