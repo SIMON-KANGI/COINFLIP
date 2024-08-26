ef705cb767adde4dca66942b76c1440fbdfe4b9d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-26 18:14:45 +0300

    coin flip function

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -8,26 +8,27 @@ function Coin({ selectedSide, flipCoin, wallet,result }) {
     setIsFlipping(true);
 
     setTimeout(() => {
-      const result = flipCoin(); // This should return 'Heads' or 'Tails'
+      const result = flipCoin();
+      console.log(result) // This should return 'Heads' or 'Tails'
       setFlippedSide(result); // Set the flipped side to the result of the coin flip
       setIsFlipping(false);
     }, 1000); // Match the timeout with the duration of the animation
   };
-console.log(flippedSide)
+
   return (
     <div>
       <div>
         <h2>Choose a side:</h2>
         <p className='text-white text-xl'>{selectedSide ? `You chose: ${selectedSide}` : 'Click the flip button to start'}</p>
         <img
-          src={`/${result|| selectedSide}.jpg`} // Show the flipped side if available, otherwise the selected side
+          src={`/${flippedSide || selectedSide}.jpg`} // Show the flipped side if available, otherwise the selected side
           alt={flippedSide || selectedSide}
           className={`w-36 h-36 rounded-full ${isFlipping ? 'flip-animation' : ''}`}
         />
       </div>
       <button
         className="px-8 m-5 py-3 bg-stone-950 text-white rounded-md"
-        onClick={flipCoin}
+        onClick={handleFlipCoin}
         disabled={!wallet.connected || isFlipping}
       >
         Flip Coin
